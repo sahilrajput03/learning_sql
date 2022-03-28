000835717f8f744c4508abf5ba6569750aed96b5
Sahil Rajput <sahilrajput03@gmail.com>
2022-03-28 15:38:52 +0530

    Fix files.

--- with-sequelize/app.js
+++ with-sequelize/app.js
@@ -1,18 +1,18 @@
-require("colors");
-const sequelize = require("./sz.js");
+require('colors')
+const sequelize = require('./sz.js')
 
 async function main() {
-  try {
-    await sequelize.authenticate();
-    console.log("\n:: ---- CONNECTED\n".yellow.bold);
+	try {
+		await sequelize.authenticate()
+		console.log('\n:: ---- CONNECTED\n'.yellow.bold)
 
-    await require("./1")();
+		await require('./1')()
 
-    await sequelize.close(); // close the connection.
-    console.log("\n:: ---- DISCONNECTED\n".yellow.bold);
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+		await sequelize.close() // close the connection.
+		console.log('\n:: ---- DISCONNECTED\n'.yellow.bold)
+	} catch (error) {
+		console.error('Unable to connect to the database:', error)
+	}
 }
 
-main();
+main()
