1d2eceade0afe148a9c67bd8432c60c73fa4b578
Sahil Rajput <sahilrajput03@gmail.com>
2022-04-04 02:21:54 +0530

    up

--- todo-app/README.md
+++
@@ -1,6 +0,0 @@
-# Readme
-
-Reference projects
-
-- [les-chat](https://github.com/amand33p/les-chat)
-- [bug-tracker](https://github.com/amand33p/bug-tracker)
--- todo-app/db.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize, DataTypes} = require('sequelize')
-
-const db = new Sequelize({
-	dialect: 'sqlite',
-	storage: 'database.sqlite', // this is the filename.
-})
-
-module.exports = db
