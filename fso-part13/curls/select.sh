cadedd4c0bb543cce28378335258cfdb4441ea2a
Sahil Rajput <sahilrajput03@gmail.com>
2022-05-15 18:52:04 +0530

    Update select script.

--- fso-part13/curls/select.sh
+++ fso-part13/curls/select.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-PS3='ENTER TASK NUMBER: '
+PS3='
+----
+ENTER TASK NUMBER: '
 # In below command I have added `clear`, `ls` commands to clear screen and list commands. Yo!
 # USAGE:
-# - Press Enter anytime to print the options menu!
-# - Press 1 to clear screen anytime.
+# - Press <ENTER> anytime to PRINT the options menu anytime!
+# - Press 1 to CLEAR screen anytime.
+# - Press ctrl+c to CLOSE the select menu anytime.
 # src: https://linuxize.com/post/bash-select/
-select script in 'Clear Screen' $(ls $(dirname "$0"))
+select script in 'Clear Screen' $(ls $(dirname "$0" ) | grep -v select )
 do
 	case $script in
 		'Clear Screen')
