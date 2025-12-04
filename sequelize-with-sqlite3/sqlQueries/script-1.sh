67d7c4494bd7cb987d2c004db3231830c276ba72
Sahil Rajput <sahilrajput03@gmail.com>
2025-12-04 22:12:29 +0530

    Update.

--- sequelize-with-sqlite3/sqlQueries/script-1.sh
+++ sequelize-with-sqlite3/sqlQueries/script-1.sh
@@ -2,18 +2,18 @@
 
 dbFile=db2.sqlite
 
-# Create table
+# Learn: Create table
 # sqlite3 $dbFile "CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT);"
 
-# Insert user, Get all users
+# Learn: Insert user, Get all users
 # sqlite3 $dbFile \
 # "INSERT INTO users (name) VALUES ('Bob'); \
 # SELECT * FROM users;"
 
-# Get all users
+# Learn: Get all users
 # sqlite3 $dbFile "SELECT * FROM users;"
 
-# Create Table, Insert, Get all users
+# Learn: Create Table, Insert, Get all users
 rm $dbFile
 sqlite3 $dbFile "CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT); \
 INSERT INTO users (name) VALUES ('Alice'); \
